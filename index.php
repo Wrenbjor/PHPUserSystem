abcd8d9766d5933e396c0758b3f099818d5825a9
Wayne Renbjor <Wrenbjor@gmail.com>
2011-08-04 23:26:21 -0400

    Updated Index Page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP User System</title>
+    <link rel="stylesheet" href="css/reset.css">
+</head>
+<body>
+    <h2>PHP User System Empty Page</h2>
+</body>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+<script>
+$(document).ready(function()
+{
+    
+});
+</script>
+</html>
